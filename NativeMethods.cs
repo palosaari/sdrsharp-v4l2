1796df5ea3d3a7655ad3b26639873484a5fd5f64
Antti Palosaari <crope@iki.fi>
2014-01-24 04:54:32 +0200

    V4L2: drop version number from libc filename
    Signed-off-by: Antti Palosaari <crope@iki.fi>

--- NativeMethods.cs
+++ NativeMethods.cs
@@ -6,7 +6,7 @@ namespace SDRSharp.V4L2
 	public class NativeMethods
 	{
 		private const string LibV4L2 = "v4l2";
-		private const string LibC = "c-2.18";
+		private const string LibC = "c";
 
 		// TODO: size 32?
 		[StructLayout(LayoutKind.Explicit, Size = 32)]
